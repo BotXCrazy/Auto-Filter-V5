e6b4916619ec008e220f841526b0e681c1b8185b
BotXCrazy <129079578+BotXCrazy@users.noreply.github.com>
2023-04-03 18:59:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://brokenboy:brokenboy204@cluster0.mwysc4u.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Brokenmovies204")
+DATABASE_NAME = environ.get('DATABASE_NAME', "brokenmovies204")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'CONNECTION')
 
 # Channel Button Links
